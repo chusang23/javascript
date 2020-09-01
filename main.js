d7e0461db2fdbcd44de11ad64858b1041505dcd1
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-09-01 23:41:26 +0900

    sum

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-'use strict';
+const num = 1;
+const num2 = 2;
+const result = num + num2;
+console.log(result);
 
-let globalName = 'global Name';
-{
-let name = 'elile';
-name = 'hello';
-console.log(name);
-console.log(globalName);
+const num3 = 1;
+const num4 = 2;
+const result2 = num3 + num4;
+
+function add(num1, num2) {
+    return num1, num2;
 }
 
-{
-    age = 4;
-    var age;
-}
-
-console.log(age);
-
-const daysInWeek = 7;
-const maxNumber = 5;
+const sum = add(3, 4);
