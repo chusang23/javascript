71d3abc9b95fe068f2fdbfd75c466d692a7ca8ab
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-09-02 18:39:03 +0900

    data type

---
+++ variable.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let name = 'ellie';
+console.log(name);
+name = 'hello';
+console.log(name);
+
+const daysInWeek = 7 ;
+const maxNumber = 5;
+
+const infinity = 1 / 0;
+const negativeinfinity = - 1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeinfinity);
+console.log(nAn);
