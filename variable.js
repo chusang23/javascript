d0f9c311c104629a38b41baabf52fecf384a09b3
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-12-02 16:01:17 +0900

    use strict

--- variable.js
+++ variable.js
@@ -1,16 +1,10 @@
 'use strict';
-
+let globalName = 'global name';
+{
 let name = 'ellie';
 console.log(name);
 name = 'hello';
 console.log(name);
-
-const daysInWeek = 7 ;
-const maxNumber = 5;
-
-const infinity = 1 / 0;
-const negativeinfinity = - 1 / 0;
-const nAn = 'not a number' / 2;
-console.log(infinity);
-console.log(negativeinfinity);
-console.log(nAn);
+}
+console.log(name);
+console.log(globalName);
