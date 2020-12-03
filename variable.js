554799fd9bb1c7adf9fa419bcaa38efe66c7024e
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-12-03 23:33:26 +0900

    note

--- variable.js
+++ variable.js
@@ -9,6 +9,26 @@ console.log(name);
 console.log(name);
 console.log(globalName);
 
+// number - special numberic values = infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`;
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
 let text = 'hello';
 console.log(text.charAt(0));
 console.log(`value: ${text}, type: ${typeof text}`);
