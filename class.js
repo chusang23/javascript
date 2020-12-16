66b08770ed6340805a5cc449c32079349bad7fb7
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-12-16 23:50:20 +0900

    note

--- class.js
+++ class.js
@@ -25,3 +25,36 @@
  console.log(ellie.name);
  console.log(ellie.age);
  ellie.speak();
+
+ // 2. Getter and setters
+ class User {
+     constructor(firstName, lastName, age) {
+         this.firstName = firstName;
+         this.lastName = lastName;
+         this.age = age;
+     }
+
+     get age() {
+         return this._age;
+     }
+
+     set age(value) {
+         // if (value < 0) {
+         // throw Error('age can not be negative');
+         // }
+         this._age = value < 0 ? 0 : value;
+    }
+ }
+
+ const user1 = new User('Steve', 'Job', -1);
+ console.log(user1.age);
+
+ // 3. Fields (public, private)
+ // Too soon!
+ class Experiment {
+     publicField = 2;
+     #privateField = 0;
+ }
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
